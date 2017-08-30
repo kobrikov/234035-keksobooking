30151fe58c89fe448e6d5be6a86d51c110f3b90c
Evgeniy Kobrikov <kobrikov@yandex.ru>
2017-08-30 13:19:30 +0300

    Fixed remarks

--- js/data.js
+++ js/data.js
@@ -1,6 +1,13 @@
 'use strict';
 
 window.data = (function () {
+  var AD_AVATARS = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
+  var AD_TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+  var AD_TYPES = ['flat', 'house', 'bungalo'];
+  var AD_CHECKIN_CHECKOUT = ['12:00', '13:00', '14:00'];
+  var AD_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+  var COUNT_ADS = 8;
+
   var getRandomArray = function (array) {
     var newArray = [];
     var randomCount = getRandomNum(array.length);
@@ -32,11 +39,11 @@ window.data = (function () {
     return Math.round(min + Math.random() * (max - min));
   };
 
-  var getAds = function (avatars, titles, types, check, features, count) {
+  var getAds = function () {
     var adsArray = [];
-    var avatarsArray = avatars.slice();
-    var titlesArray = titles.slice();
-    for (var i = 0; i < count; i++) {
+    var avatarsArray = AD_AVATARS.slice();
+    var titlesArray = AD_TITLES.slice();
+    for (var i = 0; i < COUNT_ADS; i++) {
       var locX = getRandomFromRange(300, 900);
       var locY = getRandomFromRange(100, 500);
 
@@ -49,12 +56,12 @@ window.data = (function () {
           title: getNoRepeatValue(titlesArray),
           address: locX + ', ' + locY,
           price: getRandomFromRange(1000, 1000000),
-          type: types[getRandomNum(types.length)],
+          type: AD_TYPES[getRandomNum(AD_TYPES.length)],
           rooms: getRandomFromRange(1, 5),
           guests: getRandomFromRange(1, 8),
-          checkin: check[getRandomNum(check.length)],
-          checkout: check[getRandomNum(check.length)],
-          features: getRandomArray(features),
+          checkin: AD_CHECKIN_CHECKOUT[getRandomNum(AD_CHECKIN_CHECKOUT.length)],
+          checkout: AD_CHECKIN_CHECKOUT[getRandomNum(AD_CHECKIN_CHECKOUT.length)],
+          features: getRandomArray(AD_FEATURES),
           description: '',
           photos: []
         },
@@ -69,7 +76,7 @@ window.data = (function () {
   };
 
   return {
-    create: getAds
+    get: getAds
   };
 
 })();
--- js/map.js
+++ js/map.js
@@ -2,19 +2,13 @@
 
 (function () {
   var pinsMap = document.querySelector('.tokyo__pin-map');
-  var AD_AVATARS = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
-  var AD_TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-  var AD_TYPES = ['flat', 'house', 'bungalo'];
-  var AD_CHECKIN_CHECKOUT = ['12:00', '13:00', '14:00'];
-  var AD_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  var COUNT_ADS = 8;
 
-  var ads = window.data.create(AD_AVATARS, AD_TITLES, AD_TYPES, AD_CHECKIN_CHECKOUT, AD_FEATURES, COUNT_ADS);
-  window.pin.renderPin(ads);
-  window.dialog.renderDialog(ads[0]);
+  var advertisements = window.data.get();
+  window.pin.renderPin(advertisements);
+  window.dialog.renderDialog(advertisements[0]);
 
   var activePins = pinsMap.querySelectorAll('.pin:not(.pin__main)');
-  ads.forEach(function (element, i) {
+  advertisements.forEach(function (element, i) {
     activePins[i].addEventListener('click', function (evt) {
       window.pin.setActiveClick(evt);
       window.dialog.renderDialog(element);
