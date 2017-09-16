946f072f52b3a45bbcfc7c13d6126632e7abeec4
Evgeniy Kobrikov <kobrikov@yandex.ru>
2017-09-16 20:17:34 +0300

    Final fixes

--- js/show-dialog.js
+++ js/show-dialog.js
@@ -1,20 +1,35 @@
 'use strict';
 
 window.showDialog = (function () {
-  return function (array) {
-    var pinsMap = document.querySelector('.tokyo__pin-map');
-    var activePins = pinsMap.querySelectorAll('.pin:not(.pin__main)');
-    array.forEach(function (element, i) {
-      activePins[i].addEventListener('click', function (evt) {
-        window.pin.setActiveClick(evt, element);
-        window.dialog.render(element);
-      });
-      activePins[i].addEventListener('keydown', function (evt) {
-        if (window.util.isEnterPress(evt)) {
-          window.pin.setActiveClick(evt);
-          window.dialog.render(element);
-        }
-      });
+  var pinsMap = document.querySelector('.tokyo__pin-map');
+  var activePins;
+  var data;
+
+  var onClickPin = function (evt) {
+    for (var i = 0; i < activePins.length; i++) {
+      if (activePins[i] === evt.currentTarget) {
+        break;
+      }
+    }
+    window.pin.setActiveClick(evt);
+    window.dialog.render(data[i]);
+  };
+  var onPressPin = function (evt) {
+    if (window.util.isEnterPress(evt)) {
+      onClickPin(evt);
+    }
+  };
+  var showDialog = function (array) {
+    data = array;
+    activePins = pinsMap.querySelectorAll('.pin:not(.pin__main)');
+    [].forEach.call(activePins, function (element) {
+      element.addEventListener('click', onClickPin);
+      element.addEventListener('keydown', onPressPin);
     });
+  };
+  return {
+    open: showDialog,
+    click: onClickPin,
+    press: onPressPin
   };
 })();
